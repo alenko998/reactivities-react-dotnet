925407fc815022a367e0d08fbe6a1b959d4f7f23
Alen Smrkovic <alensmrkovic@gmail.com>
2026-05-09 23:21:26 +0200

    Creating an API Controller

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ea1244bca760957cdd96a8558ab0d0828d28ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a5c2275dfa2bf749f43f451b8dcb8aa62cdd57f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
