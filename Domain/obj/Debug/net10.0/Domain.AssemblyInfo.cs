a5b1efca4f545fefde67c59faeaa92e27022695d
Alen Smrkovic <alensmrkovic@gmail.com>
2026-05-12 13:57:45 +0200

    Adding a Mediator handler for the Details

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26810ce35d5a1d37ffb011988339a8354b0aecc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeb368d481ef254fe0edbaa44c3d63ad2a7d6694")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
