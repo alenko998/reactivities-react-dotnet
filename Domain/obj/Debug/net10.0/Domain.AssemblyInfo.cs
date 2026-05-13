f662061bffb734849c074af0883544a02978b4dc
Alen Smrkovic <alensmrkovic@gmail.com>
2026-05-13 12:50:10 +0200

    Routing

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4002b7f8742db24c5f252d7a0e82dcbb40deab3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fce3a6e2fdb3cfaaaf8325bb35525e22e243305")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ client/src/app/router/Routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import App from "../layout/App";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import HomePage from "../../features/home/HomePage";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetail";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            { path: '', element: <HomePage /> },
+            { path: 'activities', element: <ActivityDashboard /> },
+            { path: 'activities/:id', element: <ActivityDetails /> },
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
+            { path: 'manage/:id', element: <ActivityForm /> },
+        ]
+    },
+]);
