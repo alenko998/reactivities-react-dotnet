762758b0971a21e84eb8789d1be989c3b71a4848
Alen Smrkovic <alensmrkovic@gmail.com>
2026-05-13 13:18:22 +0200

    Styling the User Interface

--- Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fce3a6e2fdb3cfaaaf8325bb35525e22e243305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f662061bffb734849c074af0883544a02978b4dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
